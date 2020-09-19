eabc30178def2f11907a4d23ef25642b96244eb5
Batin EVIRGEN <batinevirgen@gmail.com>
2020-09-19 22:10:10 +0300

    Installed express and created simple server.

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Backend server listening on port ${port}`);
+});
