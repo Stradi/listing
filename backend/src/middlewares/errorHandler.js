bdb5309b8d39a5fa929a7edc4eb26cb0208e1d80
Batin EVIRGEN <batinevirgen@gmail.com>
2020-09-22 14:35:51 +0300

    Added _mongooseErrorToJSON function in errorHandler middleware and added special condition for ValidationErrors and returning validation errors in simplified json format

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,11 +1,42 @@
+const mongoose = require("mongoose");
+
 const _errorHandler = (error, req, res, next) => {
-  //TODO: Check if error is Mongoose error
+  let json = {
+    code: error.code || "ERROR_CODE_UNKNOWN",
+    message: error.message || "ERROR_MESSAGE_UNKNOWN"
+  };
+  
+  if(error instanceof mongoose.Error) {
+    json = _mongooseErrorToJSON(error);
+  }
+  
   return res.status(error.status || 500).json({
-    error: {
-      code: error.code || "ERROR_CODE_UNKNOWN",
-      message: error.message || "ERROR_MESSAGE_UNKNOWN"
-    }
+    error: json
   });
+}
+
+const _mongooseErrorToJSON = (error) => {
+  let code = "UNKNOWN_DATABASE";
+  let message = "UNKNOWN_DATABASE";
+  let fields = [];
+
+  if(error instanceof mongoose.Error.ValidationError) {
+    code = "DATABASE_VALIDATION";
+    message = "Validation failed, check fields";
+    for(field in error.errors) {
+      fields.push({
+        name: error.errors[field].path,
+        kind: error.errors[field].kind,
+        message: error.errors[field].message
+      });
+    }
+  }
+
+  return {
+    code,
+    message,
+    fields
+  };
 }
 
 module.exports = _errorHandler;
